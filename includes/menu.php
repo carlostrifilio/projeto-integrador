4e048160ca6ea13875748cd3c302e947eff79bf4
TchescoSemdo'git config --global user.email nextelfrancisco23@gmail.com <nextelfrancisco23@gmail.com>
2019-12-12 16:00:47 -0300

    Botao home e login altearado cor e estilo de font

--- includes/menu.php
+++ includes/menu.php
@@ -21,7 +21,7 @@
                     <li><a href="#" accesskey="3" title="">Cadastrar produtos</a></li>
                     <li><a href="#" accesskey="4" title="">Cadastrar alergenos</a></li>
                     <li><a href="./login.php" accesskey="5" title="">Cadastro</a></li>
-                    <li class="current_page_item"><a href="./cadastro.php" accesskey="5" title="">Login </a></li>
+                    <li class="Login"><a href="./cadastro.php" accesskey="5" title="">Login </a></li>
                 </ul>
             </div>
         </nav>
