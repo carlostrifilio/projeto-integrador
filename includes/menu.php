1d4d0a482f88579aa625bc2d6ca3425d79744e14
Bruno Fresneda Cezero <bruno.fresneda@gmail.com>
2019-12-12 19:15:22 -0300

    feita alterações no nav e no estilo

--- includes/menu.php
+++ includes/menu.php
@@ -1,29 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
 
     <link rel="stylesheet" href="./style.css">
-</head>
-<body>
-    <nav id="wrapper">
-        <div id="header" class="container">
-            <div id="logo">
-                <h1><a href="#">O que tem dentro?</a></h1>
-            </div>
-            <div id="menu">
-                <ul>
-                    <li class="current_page_item"><a href="./index.php" accesskey="1" title="">Home</a></li>
-                    <li><a href="#" accesskey="2" title="">Consultar produtos</a></li>
-                    <li><a href="#" accesskey="3" title="">Cadastrar produtos</a></li>
-                    <li><a href="#" accesskey="4" title="">Cadastrar alergenos</a></li>
-                    <li><a href="./login.php" accesskey="5" title="">Cadastro</a></li>
-                    <li class="Login"><a href="./cadastro.php" accesskey="5" title="">Login </a></li>
-                </ul>
-            </div>
-        </nav>
-</body>
-</html>
+        <nav id="wrapper">
+            <div id="header" class="">
+                <div id="logo">
+                    <h1><a href="#">O que tem dentro?</a></h1>
+                </div>
+                <div id="menu" class="bg-light p-4 w-100 d-flex justify-content-center">
+                    <ul>
+                        <li class="current_page_item"><a href="./index.php" accesskey="1" title="">Home</a></li>
+                        <li><a href="#" accesskey="2" title="">Consultar produtos</a></li>
+                        <li><a href="#" accesskey="3" title="">Cadastrar produtos</a></li>
+                        <li><a href="#" accesskey="4" title="">Cadastrar alergenos</a></li>
+                        <li><a href="./login.php" accesskey="5" title="">Cadastre-se</a></li>
+                        <li class="Login"><a href="./cadastro.php" accesskey="5" title="">Login </a></li>
+                    </ul>
+                </div>
+            </nav>
