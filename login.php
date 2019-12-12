bb9b1b5974e1173010ecbaf04813a64746e9e8b3
Bruno Fresneda <bruno.fresneda@gmail.com>
2019-12-12 11:54:12 -0300

    adicionado lista de ingredientes no cadastro

--- login.php
+++ login.php
@@ -1,16 +1,19 @@
-
-    
-    <?php include('./includes/menu.php');
+<?php include('./includes/menu.php');
            ?>
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/login.css">
-<main class="container d-grid">
+</head>
+  <body>     
+
+<main class="container">
 
 
 <form action=" " method="post">
-    <form >
+    <form>
     <div class=" form-row">
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-6 form-control-lg">
       <label for="Nome">Nome</label>
       <input type="text" class="form-control" id="nome" placeholder="">
     </div>
@@ -75,15 +78,50 @@
     </div>
   </div>
 
-  
+  <div class="form-group col-md-10">
+      <label for="Ingredientes">Ingredientes</label>
+      <select id="Ingredientes" class="selectpicker">
+        <option selected>Selecione seus Alergenos</option>
+        <option>Farinha de trigo</option> 
+        <option>crustáceos</option> 
+        <option>ovos</option> 
+        <option>peixes</option> 
+        <option>amendoim</option> 
+        <option>soja</option> 
+        <option>leites de todas as espécies de animais mamíferos</option> 
+        <option>amêndoa</option> 
+        <option>avelãs</option> 
+        <option>castanha de caju</option> 
+        <option>castanha-do-brasil ou castanha-do-pará</option> 
+        <option>macadâmias</option> 
+        <option>nozes</option> 
+        <option>pecãs</option> 
+        <option>pistaches</option> 
+        <option>pinoli</option> 
+        <option>castanhas</option> 
+        <option>látex natural</option> 
+        <option> centeio</option> 
+        <option>cevada</option> 
+        <option> aveia e suas estirpes hibridizadas</option> 
 
+        
+      </select>
+    </div>
 
-  </div>
+</div>
   <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
 
 
-</form>
 
 
 </main>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+
+    
+
